b62b0e3875cb82eae9204acbfc1e6cd3a33a0ff1
Deaa <deaa333@gmail.com>
2026-05-13 18:26:40 +0300

    Implement data communication btw route and view in laravel 13

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get(uri: '/', action: function (){
+    return view(view: 'welcome');
+});
+
+Route::get(uri: '/about', action: function () {
+    $name = 'Diaa';
+
+    $departments = [
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+    ];
+
+
+    //return view('about', ['name' => $name]);
+    // return view('about')->with('name', $name);
+    return view(view: 'about', data: compact( 'name', 'departments' ));
+});
+
+
+Route::post(uri: '/about', action: function(){
+    $name = $_POST['name'];
+    $departments = [
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+    ];
+
+    return view(view: 'about', data: compact( 'name',  'departments'));
 });
